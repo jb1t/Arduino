1e384ef5f352e7245f13a80eac931bbcab91cb78
Jeff Garrison <jeff.l.garrison@gmail.com>
2013-02-06 14:23:04 -0800

    refactored code after learning more about sketch
    dynamically determine the number of pins based on the ledPins array
    removed comments
    removed the shifting of ints to use the bitRead method

--- bit_number/bit_number.ino
+++ bit_number/bit_number.ino
@@ -1,27 +1,15 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
 int ledPins[] = {2,3,4,5};
-int numberOfPins = 4;
+int numberOfPins = sizeof(ledPins)/sizeof(int);
 
-int on_delay = 1000;
+int on_delay = 500;
 
-// the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
   for(int i=0; i < numberOfPins; i++)
   {
     pinMode(ledPins[i], OUTPUT); 
   }
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
   
   for(int i=0; i<10; i++)
@@ -33,20 +21,17 @@ void loop() {
 
 void displayNumber(int number)
 {
-  int tmpNumber = number;  
     for(int j=0; j<numberOfPins; j++)
     {
-      if((tmpNumber&1) == 1)
+      if(bitRead(number, j) == 1)
       {
         digitalWrite(ledPins[j], HIGH); 
       }
-      tmpNumber = tmpNumber >> 1;
     }
     delay(on_delay);
 }
 
 void resetPins() {
- 
  for(int i=0; i<numberOfPins; i++)
  {
    digitalWrite(ledPins[i], LOW);
