febc54ebcf9a17685023494c0583ad56132bfda0
Jeff Garrison <jeff.l.garrison@gmail.com>
2013-02-10 19:21:38 -0800

    Added ResetGame() and countOfMissedPresses
    Basically if you missed the "goal" light 3 times (maxMissedPresses), the
    game will reset. It will also display the current score and the
    highscore at the end of the game.

--- led_game/led_game.ino
+++ led_game/led_game.ino
@@ -1,12 +1,17 @@
 int ledPins[] = {2,3,4,5};
 int numberOfPins = sizeof(ledPins)/sizeof(int);
-int ledDelay = 250;
-const int decrementDelay = 2;
-int minDelay = 25;
 
-int buttonPin = 12;
-int currentLEDIndex = 0;
-int goalLEDIndex = 3;
+const int decrementDelay = 25;
+const int minDelay = 25;
+const int buttonPin = 12;
+const int goalLEDIndex = 3;
+const int maxMissedPresses = 3;
+String scoreMessage = "Your score is ";
+String highScoreMessage = "High score is ";
+String oopsMessage = "Oops number ";
+
+//variables that change during game play
+int score, countOfMissedPresses, currentLEDIndex, ledDelay, highScore = 0;
 
 void setup() {               
  Serial.begin(9600); 
@@ -15,6 +20,7 @@ void setup() {
   {
    pinMode(ledPins[i], OUTPUT); 
   }
+  ResetGame();
 }
 
 void loop() { 
@@ -23,17 +29,45 @@ void loop() {
   {
     if(currentLEDIndex == goalLEDIndex)
     {
-      Serial.println("Button pressed on the GOAL button!");
+      score++;
       ledDelay -= decrementDelay;
       if(ledDelay < minDelay)
         ledDelay = minDelay;
+    }
+    else
+    {
+      countOfMissedPresses++;
+      if(countOfMissedPresses >= maxMissedPresses)
+      {
+       Serial.println(scoreMessage + score);
+       ResetGame(); 
+       DisplayHighScore();
+      }
+      else
+      {
+       Serial.println(oopsMessage + countOfMissedPresses); 
+      }
     }
     BlinkLED(currentLEDIndex, 200);
     BlinkLED(currentLEDIndex, 200);  
   }
   BlinkLED(currentLEDIndex, ledDelay);
-  IncrementLEDIndex();
-  
+  IncrementLEDIndex();  
+}
+
+void DisplayHighScore()
+{
+   Serial.println(highScoreMessage + highScore); 
+}
+
+void ResetGame()
+{
+ if(score > highScore)
+   highScore = score;
+ currentLEDIndex = 0;
+ ledDelay = 250;
+ score = 0;
+ countOfMissedPresses = 0;
 }
 
 void IncrementLEDIndex()
@@ -45,12 +79,10 @@ boolean IsButtonPressed()
 { 
   if(digitalRead(buttonPin) == 0)
   {
-    Serial.println("Button pressed!");
     return true;
   }
   else
   {
-    Serial.println("Button NOT pressed");
     return false;
   }
 }
