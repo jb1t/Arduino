1dd490dc5f64b3cdae10c85d226096f2d35bfb35
Jeff Garrison <jeff.l.garrison@gmail.com>
2013-02-10 22:23:09 -0800

    Added buzzer tones to my game
    Now you get a different sound if you clicked the correct LED vs. an
    incorrect LED
    If you get a new high score you get a little song

--- led_game/led_game.ino
+++ led_game/led_game.ino
@@ -1,11 +1,21 @@
+#include "pitches.h"
+
 int ledPins[] = {2,3,4,5};
 int numberOfPins = sizeof(ledPins)/sizeof(int);
+const int buzzerPin = 8;
+const int buttonPin = 12;
 
+const int buzzerDuration = 500;
 const int decrementDelay = 25;
 const int minDelay = 25;
-const int buttonPin = 12;
 const int goalLEDIndex = 3;
 const int maxMissedPresses = 3;
+const unsigned int successTone = 2027;
+const unsigned int failureTone = 1027;
+
+int successMusicNote[] = {NOTE_C6, NOTE_C6, NOTE_G6, NOTE_G6, NOTE_A6, NOTE_A6, NOTE_G6, NOTE_F6, NOTE_F6, NOTE_E6, NOTE_E6, NOTE_D6, NOTE_D6, NOTE_C6}; //ccggaagffeeddc
+int successMusicNoteDuration[] = {8,8,8,8,8,8,4,8,8,8,8,8,8,4};
+
 String scoreMessage = "Your score is ";
 String highScoreMessage = "High score is ";
 String oopsMessage = "Oops number ";
@@ -29,6 +39,7 @@ void loop() {
   {
     if(currentLEDIndex == goalLEDIndex)
     {
+      tone(buzzerPin,successTone,buzzerDuration);
       score++;
       ledDelay -= decrementDelay;
       if(ledDelay < minDelay)
@@ -36,6 +47,7 @@ void loop() {
     }
     else
     {
+      tone(buzzerPin,failureTone,buzzerDuration);
       countOfMissedPresses++;
       if(countOfMissedPresses >= maxMissedPresses)
       {
@@ -55,6 +67,25 @@ void loop() {
   IncrementLEDIndex();  
 }
 
+void playSuccessMusic()
+{
+ for (int thisNote = 0; thisNote < 14; thisNote++) {
+
+    // to calculate the note duration, take one second 
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/successMusicNoteDuration[thisNote];
+    tone(buzzerPin, successMusicNote[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(buzzerPin);
+  } 
+}
+
 void DisplayHighScore()
 {
    Serial.println(highScoreMessage + highScore); 
@@ -63,7 +94,10 @@ void DisplayHighScore()
 void ResetGame()
 {
  if(score > highScore)
+ {
+   playSuccessMusic();
    highScore = score;
+ }
  currentLEDIndex = 0;
  ledDelay = 250;
  score = 0;
