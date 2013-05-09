3b4ee3d750cc1baa55755120438b51a373014240
Jeff Garrison <jeff.l.garrison@gmail.com>
2013-05-09 12:57:07 -0700

    Added an LED and Buzzer
    Added an LED and Buzzer for pre-notification of servo rotation, also
    refactored the delay between servo rotations to not use the delay()
    method, but rather use millis() and save off when to execute the next
    rotation. The advantage here is we can then press the button to make the
    servos rotate whenever we want. If we use a 12 hour delay, for example,
    no other inputs could be easily captured.

--- servo/sweep/sweep.ino
+++ servo/sweep/sweep.ino
@@ -4,21 +4,32 @@
 
 
 #include <Servo.h> 
- 
+#include "pitches.h"
+
 Servo myservo;  // create servo object to control a servo 
                 // a maximum of eight servo objects can be created 
                 
 const int buttonPin = 12;
+const int ledPin = 7;
+const int buzzerPin = 8;
+
+unsigned long lastExecution;
 
 int maxRotationSpeedClockwise = 0;
 int stopRotation = 90;
 long rotationDelayMilliseconds = 2000; // 2000 milliseconds (i.e. 2 seconds)
-long stopRotationDelayMilliseconds = 60000; // 1000 * 60 * 60 * 12; //43200000 (i.e. 12 hours)
+unsigned long pauseInterval = 43200000;// (i.e. 12 hours)
+
+const unsigned int successTone = 2027;
+const unsigned int failureTone = 1027;
+int successMusicNote[] = {NOTE_C6, NOTE_C6, NOTE_G6, NOTE_G6, NOTE_A6, NOTE_A6, NOTE_G6, NOTE_F6, NOTE_F6, NOTE_E6, NOTE_E6, NOTE_D6, NOTE_D6, NOTE_C6}; //ccggaagffeeddc
+int successMusicNoteDuration[] = {8,8,8,8,8,8,4,8,8,8,8,8,8,4};
  
 void setup() 
 { 
   //Serial.begin(9600);
   pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
   myservo.write(stopRotation);
 } 
@@ -26,10 +37,11 @@ void setup()
  
 void loop() 
 { 
-  if(IsButtonPressed())
+  if(IsButtonPressed() || (lastExecution + pauseInterval) < millis())
   {
-    RotateServosAndThenStop();
-    //delay(stopRotationDelayMilliseconds);
+    lastExecution = millis();
+    RaiseAlarm();
+    RotateServosAndThenStop();    
   }
   else
   {
@@ -39,9 +51,23 @@ void loop()
 
 void RotateServosAndThenStop()
 {
+  digitalWrite(ledPin, HIGH);
   myservo.write(maxRotationSpeedClockwise);
   delay(rotationDelayMilliseconds);
   myservo.write(stopRotation);
+  digitalWrite(ledPin, LOW);
+}
+
+void RaiseAlarm()
+{
+  playSuccessMusic();
+  for(int i=0; i<10; ++i)
+  {
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+  }
 }
 
 boolean IsButtonPressed()
@@ -56,4 +82,23 @@ boolean IsButtonPressed()
     //Serial.println("false");
     return false;
   }
+}
+
+void playSuccessMusic()
+{
+ for (int thisNote = 0; thisNote < 14; thisNote++) {
+
+    // to calculate the note duration, take one second 
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/successMusicNoteDuration[thisNote];
+    tone(buzzerPin, successMusicNote[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(buzzerPin);
+  } 
 }
